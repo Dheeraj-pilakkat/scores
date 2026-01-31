12402c94495faaf169403d13fe5ccf61d6209e89
Dheeraj <pilakkatdheeraj@gmail.com>
2026-01-31 20:37:05 +0530

    feat: reduce data polling interval from 10 seconds to 5 seconds

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
     // Poll every 10 seconds
     const intervalId = setInterval(() => {
         fetchData(true);
-    }, 10000);
+    }, 5000);
 
     return () => clearInterval(intervalId);
   }, []);
